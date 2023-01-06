299a087daa529a95bd5cadd9c59f250bba3a391d
rikucame <rikucame@gmail.com>
2023-01-06 16:47:09 +0900

    色々追加

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,8 +1,29 @@
-type Props = { params: { id: string } };
 import { getPhotosDirectories } from "utils/getPhotosDirectories";
+import fs from "fs";
+import Image from "next/image";
+import style from "./style.module.scss";
+
+type Props = { params: { id: string } };
 
 export default async function Page({ params }: Props) {
-  return <h1>{params.id}</h1>;
+  const photos = fs.readdirSync(`./public/photos/${params.id}`);
+  return (
+    <div className={style.wrap}>
+      <h1>{params.id}</h1>
+      {photos.map((photo) => (
+        <div key={photo} className={style.photo}>
+          <Image
+            src={`/photos/${params.id}/${photo}`}
+            width={500}
+            height={500}
+            alt={photo}
+            style={{ width: "75vw", height: "auto" }}
+          />
+          <p className={style.title}>{photo}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export async function generateStaticParams() {
